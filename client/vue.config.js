79373959692a4a94adb7f992f7e45f9672589c1e
Emil Pedersen <emil.pedersen@stretch.se>
2020-03-16 14:12:27 +0100

    Removed comments

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,18 +1,3 @@
-// const path = require("path");
-
-// module.exports = {
-//   outputDir: path.resolve(__dirname, "../server/public"),
-//   devServer: {
-//     proxy: {
-//       "^/api": {
-//         target: "http://localhost:8080/",
-//         ws: true,
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// };
-
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/sosRegistration/" : "/"
 };
