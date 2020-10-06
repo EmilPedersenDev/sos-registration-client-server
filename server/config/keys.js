a6632c611c52ce73ab3889310ff5dab6e287ceb8
Emil Pedersen <emil.pedersen@stretch.se>
2020-10-06 22:09:15 +0200

    testing gitignore server

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,10 +2,12 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://SosUser1:USc6MT0cqeDC6USq@sosregistration-edyry.mongodb.net/test?retryWrites=true&w=majority"
+      "mongodb+srv://SosUser1:USc6MT0cqeDC6USq@sosregistration-edyry.mongodb.net/test?retryWrites=true&w=majority",
   };
 } else {
   module.exports = {
-    mongoURI: "mongodb://localhost:27017/users"
+    mongoURI: "mongodb://localhost:27017/users",
   };
 }
+
+console.log("test");
